d302ea667b91d3a72ffe59d85a6715aaa832d809
user <user@DESKTOP-SCM1N7O>
2023-05-14 08:43:41 +0800

    2nd

---
+++ Ambulance_Service_App/src/main/java/amir/ambulance_service_app/Ambulance_Service_App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package amir.ambulance_service_app;
+
+/**
+ *
+ * @author user
+ */
+public class Ambulance_Service_App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
