29184d5b473ca09830ac767f3187c2d4a4f56691
Are-mir <95739145+Are-mir@users.noreply.github.com>
2023-05-15 16:13:55 +0800

    Add files via upload

---
+++ AmbulanceServiceProvider/src/ambulanceserviceprovider/OnlineBanking.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ambulanceserviceprovider;
+
+/**
+ * Aliaaaaaaa
+ */
+public class OnlineBanking extends Payment{
+    private String bank;
+    private boolean paymentSuccess;
+    
+    public OnlineBanking(){
+        bank = "";
+        paymentSuccess = false;
+    }
+}
